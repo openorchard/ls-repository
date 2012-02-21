605adda3e6abbbe8d102c7807a6d43bf17bcb4f1
EHLOVader <joe.at.trends+github@gmail.com>
2012-02-21 10:16:18 -0500

    Updated with strlen checks on URLs to fix empty redirects and 410 error on un routable pages, which have no new url to 301 to.

--- pages/old_url_redirect_for_miva_merchant/page_old_url_redirect_for_miva_merchant.php
+++ pages/old_url_redirect_for_miva_merchant/page_old_url_redirect_for_miva_merchant.php
@@ -3,12 +3,15 @@
 	if(Phpr::$request->getField('Screen') == 'PROD'
 	&& $product = Shop_Product::create()->where(' shop_products.sku = ?',Phpr::$request->getField('Product_Code'))->find_all())
 	{
-	 	PHPr::$response->redirect($product->page_url('store/product'),true);
+		if(strlen($product->page_url('store/product')))
+	 		PHPr::$response->redirect($product->page_url('store/product'),true);
 
 	}elseif(Phpr::$request->getField('Screen') == 'CTGY'
 	&& $category = Shop_Category::create()->find_by_code(Phpr::$request->getField('Category_Code')))
 	{
-		PHPr::$response->redirect($category->page_url('store/category'),true);
+		if(strlen($category->page_url('store/category')))
+	 		PHPr::$response->redirect($category->page_url('store/category'),true);
+
 	}elseif(Phpr::$request->getField('Screen') == 'SRCH'
 	&& $search = Phpr::$request->getField('Super:search'))
 	{
@@ -18,8 +21,11 @@
 
 	}
 
-
+	//More permanent solution which says the page no longer exists.
+	header("HTTP/1.0 410 Gone");
+	//header("HTTP/1.0 404 Not Found");
 	?>
+	
 	<div class="grid_16">
 	  <h4> Page has moved</h4>
 	  This page has moved or no longer exists. You may be using an old link, or bookmark. Try heading back to the <a href="<?php echo root_url('/') ?>">homepage</a> to see if you can find what you are looking for from there.
